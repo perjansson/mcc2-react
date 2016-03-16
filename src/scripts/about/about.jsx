830437840b835b0e6c9fab31053139aca7dc1580
Per Jansson <per.r.jansson@gmail.com>
2016-03-16 12:51:57 +0100

    trying image path

--- src/scripts/about/about.jsx
+++ src/scripts/about/about.jsx
@@ -6,7 +6,7 @@ export default class Toplist extends React.Component {
       <div>
         <div className="row about">
           <div className="col-xs-12 col-md-5">
-            <img src={'/src/images/yo_dawg.jpg'} alt="yo_dawg" className="center-block img-responsive img-rounded"/>
+            <img src={'./src/images/yo_dawg.jpg'} alt="yo_dawg" className="center-block img-responsive img-rounded"/>
           </div>
           <div className="col-xs-12 col-md-7">
             I'm building Meeting cost calculator mostly to try out and learn different frameworks, techniques and patterns. But also little bit because I think quite a lot of meetings are a waste of time. Some of them are good, some of them are necessary, but some of them are just a waste of time and money.
