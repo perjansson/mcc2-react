a521f4e1252cd0bbf97187b8cd938512245ff251
Per Jansson <per.r.jansson@gmail.com>
2016-03-16 12:38:03 +0100

    sub-header

--- src/scripts/header.jsx
+++ src/scripts/header.jsx
@@ -5,6 +5,9 @@ export default class Header extends React.Component {
     return (
 	    <header>
 	      <h1>Meeting Cost Calculator <strong>2</strong></h1>
+        <div className="sub-header text-right">
+          <small>Build with <a href="https://facebook.github.io/react/" target="_blank">React</a> and ES2015</small>
+        </div>
 	    </header>
     );
   }
