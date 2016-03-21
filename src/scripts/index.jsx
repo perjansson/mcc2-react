12e44ba6503d00445c52b55a043a7a140cc79898
Per Jansson <per.r.jansson@gmail.com>
2016-03-21 20:45:52 +0100

    eslint

--- src/scripts/index.jsx
+++ src/scripts/index.jsx
@@ -1,11 +1,11 @@
-require("../../node_modules/bootstrap/dist/css/bootstrap.min.css");
-require("../../node_modules/animate.css/animate.min.css");
-require("../../node_modules/font-awesome/css/font-awesome.min.css");
-require("../../node_modules/react-select/dist/react-select.min.css");
-require("../styles/style.css")
+require('../../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('../../node_modules/animate.css/animate.min.css');
+require('../../node_modules/font-awesome/css/font-awesome.min.css');
+require('../../node_modules/react-select/dist/react-select.min.css');
+require('../styles/style.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Header from './common/header.component';
 import Navigation from './common/navigation.component';
 import Meeting from './meeting/meeting.component';
@@ -13,24 +13,35 @@ import TopList from './toplist/toplist.component';
 import About from './about/about.component';
 
 export class App extends React.Component {
-	render() {
-		return (
+  render() {
+    return (
       <div className="container text-center animated bounceInDown">
-				<Header/>
-				<Navigation/>
-        {this.props.children}
-			</div>
-		);
-	}
+        <Header/>
+        <Navigation/> {this.props.children}
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  children: React.PropTypes.node.isRequired,
+};
 
 ReactDOM.render((
   <Router history={hashHistory}>
-    <Route name="app" path="/" component={App}>
-			<IndexRoute component={Meeting} />
-	    <Route name="meeting" path="/meetings/new" component={Meeting}/>
-	    <Route name="meetings" path="/meetings" component={TopList}/>
-	    <Route name="about" path="/about" component={About}/>
-		</Route>
+    <Route component={App}
+        name="app"
+        path="/">
+      <IndexRoute component={Meeting}/>
+      <Route component={Meeting}
+          name="meeting"
+          path="/meetings/new"/>
+      <Route component={TopList}
+          name="meetings"
+          path="/meetings"/>
+      <Route component={About}
+          name="about"
+          path="/about"/>
+    </Route>
   </Router>
-), document.querySelector("#mccApp"))
+), document.querySelector('#mccApp'));
